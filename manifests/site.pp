c72bbffbfcec1426c3acdcd16095bc8b57c1f462
luizfrmoraes <68019016+luizfrmoraes@users.noreply.github.com>
2021-10-21 09:43:52 -0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,7 +9,7 @@ node 'master.puppet.vm' {
   include role::master_server
   file {'/root/README':
     ensure => file,
-    content => "Managed by puppet server version ${fqdn}",
+    content => "Welcome to ${fqdn}",
     owner => 'root',
   }
 }
