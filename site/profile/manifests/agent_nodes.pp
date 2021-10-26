6474e44ca20dcdd3f398853112e626ebc79c5890
luizfrmoraes <68019016+luizfrmoraes@users.noreply.github.com>
2021-10-26 10:46:54 -0300

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -5,10 +5,14 @@ class profile::agent_nodes {
   dockeragent::node { 'minetest.puppet.vm': }
   host {'web.puppet.vm':
     ensure => present,
-    ip => '172.18.0.2',
+    ip => '172.18.0.3',
   }
   host {'db.puppet.vm':
     ensure => present,
-    ip => '172.18.0.3',
+    ip => '172.18.0.4',
+  }
+  host {'minetest.puppet.vm':
+    ensure => present,
+    ip => '172.18.0.2',
   }
 }
